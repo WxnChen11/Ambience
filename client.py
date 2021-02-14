7f50a1b8b7f5b2bda1b638e904394d4d42582caf
Winston Chen <wx.chen@mail.utoronto.ca>
2021-02-14 08:03:36 +0000

    Bot now leaves and joins automatically

--- client.py
+++ client.py
@@ -5,9 +5,8 @@ import argparse
 import shlex
 import json
 
-
 class ConfigClient():
-    def __init__(self, bot_name, config_file='config.json'):
+    def __init__(self, bot_id, config_file='config.json', default_config_file='default_config.json'):
         self.parser = argparse.ArgumentParser(description='Parse config')
         self.parser.add_argument(
             "--text_channel",
@@ -22,7 +21,10 @@ class ConfigClient():
 
         self.config_file = config_file
         self.latest = self._get_config()
-        self.name = bot_name
+        self.bot_id = bot_id
+
+        with open(default_config_file) as f:
+            self.default_config = json.load(f)
 
     def parse(self, msg):
         return self.parser.parse_args(shlex.split(msg))
@@ -35,17 +37,18 @@ class ConfigClient():
         if guild_id not in config:
             config[guild_id] = {}
 
-        config[guild_id][self.name] = new_config
+        config[guild_id][self.bot_id] = new_config
         with open(self.config_file, 'w') as f:
             json.dump(config, f)
 
         self.latest = config
 
     def get_config(self, guild_id=None):
+        guild_id = str(guild_id)
         if guild_id is None:
             return self.latest
 
-        return self.latest.get(guild_id, {}).get(self.name, None)
+        return self.latest.get(guild_id, self.default_config).get(self.bot_id, None)
 
     def _get_config(self):
         with open(self.config_file) as f:
@@ -57,6 +60,7 @@ class ConfigClient():
 class AmbienceClient(discord.Client):
     def __init__(self,
                  bot_name,
+                 bot_id,
                  music_handler=None,
                  visual_handler=None,
                  logging_dir='ambience.log'):
@@ -65,12 +69,14 @@ class AmbienceClient(discord.Client):
 
         super().__init__(intents=intents)
         self.bot_name = bot_name
+        self.bot_id = bot_id
         self.prefix = "({0})".format(bot_name)
 
         self.pp = pprint.PrettyPrinter(indent=2)
         self._initialize_logging(logging_dir)
         self.music_handler = music_handler
-        self.config_client = ConfigClient(bot_name, "{0}_config.json".format(bot_name.replace(' ', '_')))
+        self.config_client = ConfigClient(
+            str(bot_id), "{0}_config.json".format(bot_name.replace(' ', '_')))
 
     async def on_ready(self):
         print('We have logged in as {0.user}.'.format(self))
@@ -92,26 +98,57 @@ class AmbienceClient(discord.Client):
                 args = self.config_client.parse(
                     message.content[len(self.prefix):])
                 new_config = vars(args)
-                self.config_client.update_config(new_config, message.author.guild.id)
+                self.config_client.update_config(new_config,
+                                                 message.author.guild.id)
 
                 await message.channel.send(
                     'The config was updated successfully. :)')
             except Exception as e:
                 print('error when updatin config', e)
                 await message.channel.send(
-                    'The config submitted was invalid, or the update failed. :('
+                    'The config submitted was invalid, or the update failed: ' + str(e) + '.' 
                 )
 
     async def on_voice_state_update(self, member, before, after):
         # Assume events are processed more or less synchronously... too lazy to implement this otherwise.
-        # We just check if we need to join the room.
-        # Leaving is taken care of by the periodic task.
+        # Check if we need to join the room, or leave the room.
 
         if after.channel is not None:
-            our_name = self.config['bot name']
-            if not our_name in [x.name for x in after.channel.members]:
-                # join the room, and start playing music.
-                await after.channel.connect()
+            guild = member.guild.id
+            voice_channel_to_join = self.config_client.get_config(guild)['voice_channel']
+
+            if (after.channel.name != voice_channel_to_join):
+              return
+
+            if not self.bot_id in [x.id for x in after.channel.members]:
+                # see if we have an active connection already.
+                existing_vc = [x for x in self.voice_clients
+                    if x.guild and x.guild.id == member.guild.id]
+
+                if len(existing_vc) > 0 and existing_vc[0].channel != after.channel:
+                    await existing_vc[0].move_to(after.channel)
+                else:
+                    existing_vc.append(await after.channel.connect())
+                    # play a song
+
+                voice = existing_vc[0]
+                if not voice.is_playing():
+                    self.audio = 'test.mp3'
+                    self.voice = voice
+                    self._repeat_audio()
+                    # voice.play(self.audio, after=lambda e: self._repeat_audio(voice, self.audio))
+
+        if before.channel is not None:
+            guild = member.guild.id
+
+            if self.bot_id in [x.id for x in before.channel.members]:
+                # see if there are any remaining non-bot members.
+                nonbots = len([x for x in before.channel.members if not x.bot])
+                if nonbots == 0:
+                  existing_vc = [x for x in self.voice_clients
+                    if x.channel == before.channel]
+                  if len(existing_vc) > 0:
+                    await existing_vc[0].disconnect()
 
     def _initialize_logging(self, logging_dir):
         logger = logging.getLogger('discord')
@@ -125,3 +162,14 @@ class AmbienceClient(discord.Client):
         logger.addHandler(handler)
         self.logging_dir = logging_dir
         self.logger = logger
+
+    def _repeat_audio(self):
+        audio = discord.FFmpegPCMAudio(self.audio)
+        try: 
+          self.voice.play(audio, after=lambda e: self._repeat_audio())
+        except Exception as e:
+          print('Failed to repeat audio: ', e)
+
+
+# Add a scheduled job, every ~1h, that uses get_all_channels and the configs to find out which channels
+# it should send a picture and quote to.
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import json
 from client import AmbienceClient
 
 if __name__ == "__main__":
-  client = AmbienceClient('Ninth Street Espresso#5861')
 
-  # TODO: Create music handler
-  # TODO: Create picture handler
+  with open('name_to_id.json') as f:
+      name_to_id = json.load(f)
+
+  client = AmbienceClient(os.getenv('NAME'), name_to_id[os.getenv('NAME')])
   client.run(os.getenv('TOKEN'))
